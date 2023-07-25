5403a3f7d6d27d0528e284637c1ab0bfc1543b3e
dimingo <micalvin98@gmail.com>
2023-07-25 23:32:16 +0300

    accessing json data

--- Json/main.go
+++ Json/main.go
@@ -27,5 +27,31 @@ func main() {
 
 		log.Fatal(err)
 	}
-	fmt.Println(f)
+	f = map[string]interface{}{
+		"Name": "Wednesday",
+		"Age":  6,
+		"Parents": []interface{}{
+			"Gomez",
+			"Morticia",
+		},
+	}
+	m := f.(map[string]interface{})
+
+	for k, v := range m {
+		switch vv := v.(type) {
+		case string:
+			fmt.Println(k, "is string", vv)
+		case int:
+			fmt.Println(k, "is int", vv)
+		case float64:
+			fmt.Println(k, "is a float", vv)
+		case []interface{}:
+			fmt.Println(k, "is an array:")
+			for i, u := range vv {
+				fmt.Println(i, u)
+			}
+		default:
+			fmt.Println(k, "is a type I dont know to handle")
+		}
+	}
 }
